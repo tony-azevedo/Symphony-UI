9ec1181752dff9e8f381066683c9c1b10757efd9
Mark Cafaro <mark.cafaro@gmail.com>
2013-04-23 12:09:59 -0700

    Fixed file separator for Mac

--- editParameters.m
+++ editParameters.m
@@ -483,7 +483,7 @@ end
 
 function saveParameters(~, ~, handles)
     paramsDir = findSavedParametersDir(handles);    
-    [filename, pathname] = uiputfile([paramsDir '\*.mat'], 'Save Parameters');
+    [filename, pathname] = uiputfile([paramsDir '/*.mat'], 'Save Parameters');
     if isequal(filename, 0)
         % User selected cancel.
         return;
@@ -496,7 +496,7 @@ end
 
 function loadParameters(~, ~, handles)
     paramsDir = findSavedParametersDir(handles);
-    [filename, pathname] = uigetfile([paramsDir '\*.mat'], 'Load Parameters');
+    [filename, pathname] = uigetfile([paramsDir '/*.mat'], 'Load Parameters');
     if isequal(filename, 0)
         % User selected cancel.
         return;
@@ -547,7 +547,7 @@ end
 function dir = findSavedParametersDir(handles)
     protocolPath = which(class(handles.protocol));
     protocolDir = fileparts(protocolPath);
-    dir = [protocolDir '\saved_parameters'];
+    dir = [protocolDir '/saved_parameters'];
     if exist(dir, 'file') ~= 7
         mkdir(dir);
     end
