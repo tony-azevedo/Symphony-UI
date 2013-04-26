7b82b38c90ad252e3fc2726ab9c05e3cb0a2ce79
ala-laurila-lab <petriala.retina.lab@gmail.com>
2013-04-26 09:53:15 +0300

    fixing a previous commit error

--- SymphonyUI.m
+++ SymphonyUI.m
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 % Copyright (c) 2012 Howard Hughes Medical Institute.
 % All rights reserved.
 % Use is subject to Janelia Farm Research Campus Software Copyright 1.1 license terms.
 % http://license.janelia.org/license/jfrc_copyright_1_1.html
-
-=======
->>>>>>> 49a809563f03fbee054d260f8e4a16be854b787b
 classdef SymphonyUI < handle
     
     properties
