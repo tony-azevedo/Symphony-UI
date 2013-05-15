347a936d70f7db594eef2c4a14a5a6407ff594ca
Mark Cafaro <mark.cafaro@gmail.com>
2013-05-15 10:42:41 -0700

    Better comments in loopbackSimulation

--- Simulations/loopbackSimulation.m
+++ Simulations/loopbackSimulation.m
@@ -1,24 +1,30 @@
-% A simple loopback simulation for the SimulationDAQController. Returns all stimuli as responses (a loopback) or 
-% simulates noise if no stimuli is defined for a particular channel.
+% Returns all stimuli on output channels as responses on corresponding input channels (e.g. a stimulus on ANALOG_OUT.1 
+% will return as a response on ANALOG_IN.1). Noise is simulated for input channels with no associated output channel.
 
 function input = loopbackSimulation(daq, output, timeStep)
     import Symphony.Core.*;
 
+    % Create the input Dictionary to return.
     input = NET.createGeneric('System.Collections.Generic.Dictionary', ...
         {'Symphony.Core.IDAQInputStream', 'Symphony.Core.IInputData'});
 
+    % Get all input streams (i.e. channels) associated with the DAQ controller.
     inputStreams = NET.invokeGenericMethod('System.Linq.Enumerable', 'ToList', ...
         {'Symphony.Core.IDAQInputStream'}, daq.InputStreams);
     
+    % Loop through all input streams.
     inStreamEnum = inputStreams.GetEnumerator();
-
     while inStreamEnum.MoveNext()
         inStream = inStreamEnum.Current;
         inData = [];
-
-        outStreamEnum = output.Keys.GetEnumerator();
-
+        
+        if ~inStream.Active
+            % We don't care to process inactive input streams (i.e. channels without devices).
+            continue;
+        end
+        
         % Find the corresponding output data and make it into input data.
+        outStreamEnum = output.Keys.GetEnumerator();
         while outStreamEnum.MoveNext()
             outStream = outStreamEnum.Current;
 
