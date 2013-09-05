cdba9c533a61c3265a35c2ea2b1d656a25b62aa6
Mark Cafaro <mark.cafaro@gmail.com>
2013-09-05 11:12:48 -0700

    Changed default epochQueueSize from 5 to 6

--- SymphonyProtocol.m
+++ SymphonyProtocol.m
@@ -28,7 +28,7 @@ classdef SymphonyProtocol < handle & matlab.mixin.Copyable
         allowPausing = true         % An indication if this protocol allows pausing during acquisition.
         persistor = []              % The persistor to use with each epoch.
         epochKeywords = {}          % A cell array of string containing keywords to be applied to any upcoming epochs.
-        epochQueueSize = 5          % The maximum number of epochs/intervals this protocol will queue into the epoch queue at one time.
+        epochQueueSize = 6          % The maximum number of epochs/intervals this protocol will queue into the epoch queue at one time.
         numEpochsQueued             % The number of epochs queued by this protocol in the current run.
         numEpochsCompleted          % The number of epochs completed by this protocol in the current run.
         numIntervalsQueued          % The number of intervals queued by this protocol in the current run.
